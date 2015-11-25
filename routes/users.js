6e079c2a4fa7d13f3426ecd75570c16b610ce7b0
Arik Achmad <arikarik.ae@gmail.com>
2015-11-25 07:26:12 +0700

    penambahan crud untuk user route

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,51 @@
 var express = require('express');
 var router = express();
 var crud = require('mysql-crud');
+var helperview = require('../core/helperShowJson');
 
+router.get('/id/:id', function(req, res) {
+	var user_crud = crud(req.mysql, 'ikimara_user');
+	user_crud.load({
+		id_user : req.params.id
+	}, function(err, val) {
+		var msg = err;
+		if (val.length <= 0) {
+			msg = "user ditemukan";
+		}
+		helperview.showjsontoview(res, msg, val);
+	});
 
+});
+
+router.post('/add', function(req, res) {
+	var jsonData = JSON.parse(req.body.jsondata);
+	var user_crud = crud(req.mysql, 'ikimara_user');
+	user_crud.create(jsonData, function(err, val) {
+		helperview.showjsontoview(res, err, val);
+	});
+
+});
+
+router.post('/update', function(req, res) {
+	var jsonData = JSON.stringify(req.body.jsondata);
+	var user_crud = crud(req.mysql, 'ikimara_user');
+	user_crud.update({
+		id_user : jsonData.id_user
+	}, jsonData, function(err, val) {
+		helperview.showjsontoview(res, err, val);
+	});
+
+});
+
+router.post('/delete', function(req, res) {
+	var jsonData = JSON.stringify(req.body.jsondata);
+	var user_crud = crud(req.mysql, 'ikimara_user');
+	user_crud.destroy({
+		id_user : jsonData.id_user
+	}, function(err, val) {
+		helperview.showjsontoview(res, err, val);
+	});
+
+});
 
 module.exports = router;

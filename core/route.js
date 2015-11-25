a25bad6a76a139aa02d7a181c4f0c0e9319c254b
Arik Achmad <arikarik.ae@gmail.com>
2015-11-25 07:17:18 +0700

    penambahan route

--- core/route.js
+++ core/route.js
@@ -6,11 +6,14 @@ exports.setup = function(app) {
 
 	var testing = require('../routes/_testing');
 	var postingan = require('../routes/post_data');
+	var komentar = require('../routes/post_komentar');
+	var like = require('../routes/post_like');
 //	var uploadfoto = require('../routes/post_upload_image');
 
 //	app.use('/api', testing);
 	app.use('/api/post/', postingan);
-//	app.use('/api/post/', uploadfoto);
+	app.use('/api/komentar/', komentar);
+	app.use('/api/like/', like);
 	app.use(function(req, res, next) {
 		res.type('txt').status(404).send('Tidak dapat mengakses halaman ini');
 	});

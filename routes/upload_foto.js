909725e7df848b57605c9575476faa2954767f5a
Arik Achmad <arikarik.ae@gmail.com>
2015-11-26 21:59:06 +0700

    update penyimpanan untuk upload foto

--- routes/upload_foto.js
+++ routes/upload_foto.js
@@ -1,14 +1,24 @@
 var express = require('express');
 var router = express();
+var crypto = require('crypto');
 var crud = require('mysql-crud');
 var multer = require('multer');
 var helperview = require('../core/helperShowJson');
 var path = require('path');
+
+var storage = multer.diskStorage({
+	destination : path.join(__dirname, '../uploads/images'),
+	filename : function(req, file, cb) {
+		crypto.pseudoRandomBytes(16, function(err, raw) {
+			if (err) {
+				return cb(err);
+			}
+			cb(null, raw.toString('hex') + path.extname(file.originalname));
+		});
+	}
+});
 var upload = multer({
-	dest : path.join(__dirname, '../uploads/images'),
-	rename: function (fieldname, filename) {
-	    return filename.replace(/\W+/g, '-').toLowerCase() + Date.now();
-	  }
+	storage : storage
 });
 
 router.get('/image/test', function(req, res) {
@@ -21,7 +31,14 @@ router.post('/image/upload', upload.single('image'), function(req, res) {
 	console.log(req.body);
 	console.log("-----------------");
 	console.log(req.file);
-	res.type('txt').send('welldone');
+	var user_crud = crud(req.mysql, 'ikimara_post');
+	user_crud.update({
+		id_post : req.body.id_post
+	}, {
+		gambar : req.file.filename
+	}, function(err, val) {
+		helperview.showjsontoview(res, err, val);
+	});
 });
 
 module.exports = router;

3bea5057e0a7e82e24a9d3969b2e3015ea146694
Arik Achmad <arikarik.ae@gmail.com>
2015-11-26 22:00:06 +0700

    update rute untuk postingan berdasarkan (sesuatu)

--- routes/post_data.js
+++ routes/post_data.js
@@ -3,11 +3,40 @@ var router = express();
 var crud = require('mysql-crud');
 var helperview = require('../core/helperShowJson');
 
-router.get('/all', function(req, res) {
+router.get('/show/:title', function(req, res) {
+
+	var opt;
+
+	if (req.params.title === 'all') {
+		opt = '';
+	} else if (req.params.title === 'mostliked') {
+		opt = {
+			custom : "ORDER BY suka DESC"
+		};
+
+	} else if (req.params.title === 'time') {
+		opt = {
+			custom : "ORDER BY tanggal_upload ASC"
+		};
+
+	} else if (req.params.title === 'random') {
+		opt = {
+			custom : "ORDER BY RAND()"
+		};
+	}
+
+	else {
+		var msg = "parameter ngawur";
+		helperview.showjsontoview(res, msg, '');
+		return;
+	}
+
 	var user_crud = crud(req.mysql, 'ikimara_view_post');
 	user_crud.load({}, function(err, val) {
 		helperview.showjsontoview(res, err, val);
-	});
+	}, opt);
+
+	// res.type('txt').send(req.params.title);
 
 });
 
